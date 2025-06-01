4d299b0196bb380ff6c1b7d759039284b0a19880
Herb <herbwilliams808@gmail.com>
2025-06-01 20:55:53 +0100

    Remove `API.deps.json` from repository
    Deleted the `API.deps.json` file to clean up unnecessary build artifacts and reduce repository size. This file is generated during build/publish and does not need to be tracked in version control.

--- Tests/UnitTests/obj/Debug/net8.0/UnitTests.AssemblyInfo.cs
+++ Tests/UnitTests/obj/Debug/net8.0/UnitTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff9dbfbd75f5aca834b924ad22f9c9adbb277a71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02c74e645069ef728a829965388e304ab86fc171")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
